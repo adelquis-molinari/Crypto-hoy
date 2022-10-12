7350d18086775e8c13bbb2966c427e1c168b806e
adelquis-molinari <adelkis003@gmail.com>
2022-10-12 19:02:21 -0300

    refactored home page, Global Stats component

---
+++ src/components/GlobalCryptoStats.jsx
@@ -0,0 +1,39 @@
+import { Typography, Row, Col, Statistic } from "antd";
+import millify from "millify";
+
+const { Title } = Typography;
+
+export default function GlobalCryptoStats({ globalStats }) {
+  const {
+    total,
+    totalExchanges,
+    totalMarketCap,
+    total24hVolume,
+    totalMarkets,
+  } = globalStats;
+
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={millify(total)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value={millify(totalExchanges)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value={millify(totalMarketCap)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value={millify(total24hVolume)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={millify(totalMarkets)} />
+        </Col>
+      </Row>
+    </>
+  );
+}
